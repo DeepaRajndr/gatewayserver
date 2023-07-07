ae6882f26acf2ffdaeb395796d140dc84f8e2a16
DeepaRajndr <133270634+DeepaRajndr@users.noreply.github.com>
2023-07-06 21:17:00 -0400

    Addred Filters logic to add TraceID for headers

--- src/main/java/com/dpr/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/dpr/gatewayserver/GatewayserverApplication.java
@@ -1,13 +1,39 @@
 package com.dpr.gatewayserver;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/dpr/accounts/**")
+						.filters(f -> f.rewritePath("/dpr/accounts/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/dpr/loans/**")
+						.filters(f -> f.rewritePath("/dpr/loans/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/dpr/cards/**")
+						.filters(f -> f.rewritePath("/dpr/cards/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://CARDS"))
+				.build();
 	}
 
 }
